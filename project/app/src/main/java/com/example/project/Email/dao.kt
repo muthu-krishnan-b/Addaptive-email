8fc75d7cfdb5657499dfb6153ca3b5593ffe4a3d
muthu-krishnan-b <krishharrysangai@gmail.com>
2023-04-12 15:35:31 +0530

    Addaptive mail message

---
+++ project/app/src/main/java/com/example/project/Email/dao.kt
@@ -0,0 +1,20 @@
+package com.example.emailapplication
+
+import androidx.room.*
+import com.example.project.Email
+
+@Dao
+interface EmailDao {
+
+    @Query("SELECT * FROM email_table WHERE  subject= :subject")
+    suspend fun getOrderBySubject(subject: String): Email?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEmail(email: Email)
+
+    @Update
+    suspend fun updateEmail(email: Email)
+
+    @Delete
+    suspend fun deleteEmail(email: Email)
+}
